9b3116567e90a01294dfe0572ef8b308a106eda2
MetaSilence <92494416+MetaSilence@users.noreply.github.com>
2025-11-30 12:08:03 -0500

    Update problem 2b.py
    added plotting. Can vary eta values as required. Works only for two layers, layer detection and autosolving not yet implemented.

--- problem 2b.py
+++ problem 2b.py
@@ -5,6 +5,7 @@ Created on Wed Nov  5 16:49:15 2025
 @author: nicol
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 n = [1.33, 1.5, 2, 3.5]
 ln = 0
@@ -12,8 +13,12 @@ upwave = 400
 lwave = 1400
 wavelc = 500
 
+nanoaxis = np.arange(400, 2201, 1)
+
 Q = []
-P = []
+P = [0]
+tlmda = []
+R = []
 
 def Q_solver (n, i):
     tau = (2 * n[i]) / (n[i]+n[i+1])
@@ -32,10 +37,18 @@ Q.append(Q_solver(n, 1))
 Q.append(Q_solver(n, 2))
 
 for lmda in range(400, 2201):
+    P[0] = P_solver(lmda, 650)
+    T = Q[0] @ P[0]
+    T = T @ Q[1]
+    T = T @ P[0]
+    T = T @ Q[2]
+    G = abs(T[1][0] / T[0][0])
+    R.append(G**2)
+    T = 1 - G**2
+    tlmda.append(T)
+    
+plt.plot(nanoaxis, tlmda)
+plt.plot(nanoaxis, R)
     
 
 
-
-
-
-    
