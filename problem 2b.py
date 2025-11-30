f0113067a90819c8bfc956c3040eccf87709bbe8
MetaSilence <92494416+MetaSilence@users.noreply.github.com>
2025-11-30 13:10:53 -0500

    Update problem 2b.py

--- problem 2b.py
+++ problem 2b.py
@@ -12,18 +12,23 @@ ln = 0
 upwave = 400
 lwave = 1400
 wavelc = 500
+wav = np.arange(4, 22.001, 0.01)
+indexes = np.arange(1, 6.1, 0.1)
 
-nanoaxis = np.arange(400, 2201, 1)
+
+
 
 Q = []
 P = [0]
 tlmda = []
 R = []
+power = []
 
 def Q_solver (n, i):
     tau = (2 * n[i]) / (n[i]+n[i+1])
     gamma = (n[i] - n[i+1]) / (n[i] + n[i+1])
     Q_new = (1 / tau) * np.array([[1, gamma], [gamma, 1]])
+    #print(gamma)
     return Q_new
 
 def P_solver(lmda, central_wav):
@@ -31,24 +36,37 @@ def P_solver(lmda, central_wav):
     P_new = np.array([[np.exp(delta), 0], [0, np.exp(-delta)]])
     return P_new
     
-
-Q.append(Q_solver(n, 0))
-Q.append(Q_solver(n, 1))
-Q.append(Q_solver(n, 2))
-
-for lmda in range(400, 2201):
-    P[0] = P_solver(lmda, 650)
-    T = Q[0] @ P[0]
-    T = T @ Q[1]
-    T = T @ P[0]
-    T = T @ Q[2]
-    G = abs(T[1][0] / T[0][0])
-    R.append(G**2)
-    T = 1 - G**2
-    tlmda.append(T)
+def power_solver(n):
     
-plt.plot(nanoaxis, tlmda)
-plt.plot(nanoaxis, R)
+    Q.append(Q_solver(n, 0))
+    Q.append(Q_solver(n, 1))
+    Q.append(Q_solver(n, 2))
     
+    for lmda in range(400, 2201):
+        P = [0]
+        P[0] = P_solver(lmda, 650)
+        T = Q[0] @ P[0]
+        T = T @ Q[1]        
+        T = T @ P[0]
+        T = T @ Q[2]        
+        G = abs(T[1][0] / T[0][0])
+        R.append(G**2)
+        T = 1 - G**2
+        tlmda.append(T)
+        
+    wav = np.arange(4, 22.001, 0.01)
+    nanoaxis = np.arange(400, 2201, 1)    
+    irrad = (6.16e5 / (wav ** 5 * (np.exp(2484/(wav * 10 ** 2))-1)))
+    value = np.trapz(tlmda*irrad, nanoaxis)
+    T = 0
+    tlmda.clear()
+    Q.clear()
+    P.clear()
+    return value
 
+for k in indexes:
+    n = [1.33, 1.5, k, 3.5]
+    power.append(power_solver(n))
+    
+plt.plot(indexes, power)
 
